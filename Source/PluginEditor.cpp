38247fdcace2c31dae053839e949c0f1a6d63b21
Ty Seawood <tyler@tsservices.pro>
2023-04-22 00:31:29 -0700

    added Analyzer decibel marks - cleaned up code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -149,7 +149,7 @@ juce::String RotarySliderWithLabels::getDisplayString() const
         
         if(val > 999.f)
         {
-            val /= 1000.f; //1001 / 1000 = 1.001
+            val /= 1000.f;
             addK = true;
         }
         
@@ -203,9 +203,7 @@ void ResponseCurveComponent::timerCallback()
     if( parametersChanged.compareAndSetBool(false, true) )
     {
         DBG( "params changed" );
-        //update the monochain
         updateChain();
-        //signal a repaint
         repaint();
     }
 }
@@ -224,13 +222,11 @@ void ResponseCurveComponent::updateChain()
 void ResponseCurveComponent::paint (juce::Graphics& g)
 {
     using namespace juce;
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (Colours::black);
     
     g.drawImage(background, getLocalBounds().toFloat());
     
-//    auto responseArea = getLocalBounds();
-    auto responseArea = getAnalysisArea(); // getRenderArea();
+    auto responseArea = getAnalysisArea();
     
     auto w = responseArea.getWidth();
     
@@ -307,9 +303,9 @@ void ResponseCurveComponent::resized()
     
     Array<float> freqs
     {
-        20, /*30, 40, */ 50, 100,
-        200, /*300, 400, */ 500, 1000,
-        2000, /*3000, 4000,*/ 5000, 10000,
+        20,  50, 100,
+        200, 500, 1000,
+        2000, 5000, 10000,
         20000
     };
         
@@ -331,8 +327,6 @@ void ResponseCurveComponent::resized()
 //    for( auto f : freqs )
     for( auto x : xs )
     {
-//        auto normX = mapFromLog10(f, 20.f, 20000.f);
-//        g.drawVerticalLine(getWidth() * normX, 0.f, getHeight());
         g.drawVerticalLine(x, top, bottom);
     }
     
@@ -344,11 +338,9 @@ void ResponseCurveComponent::resized()
     for ( auto gDb : gain )
     {
         auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
-//        g.drawHorizontalLine(y, 0, getWidth());
         g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u): Colours::darkgrey );
         g.drawHorizontalLine(y, left, right);
     }
-   // g.drawRect(getRenderArea());
     
     g.setColour(Colours::lightgrey);
     const int fontHeight = 10;
@@ -402,6 +394,17 @@ void ResponseCurveComponent::resized()
         g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::lightgrey  );
         
         g.drawFittedText(str, r, juce::Justification::centred, 1);
+        
+        str.clear();
+        str << (gDb -24.f);
+        
+        r.setX(1);
+        textWidth = g.getCurrentFont().getStringWidth(str);
+        r.setSize(textWidth, fontHeight);
+        g.setColour(Colours::lightgrey);
+        
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+        
     }
 }
 
@@ -409,9 +412,6 @@ juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
 {
     auto bounds = getLocalBounds();
     
-//    bounds.reduce(10, //JUCE_LIVE_CONSTANT(10),
-//                  8 //JUCE_LIVE_CONSTANT(8)
-//                  );
     bounds.removeFromTop(12);
     bounds.removeFromBottom(2);
     bounds.removeFromLeft(20);
